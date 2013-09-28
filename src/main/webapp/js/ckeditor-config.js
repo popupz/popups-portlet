567670bdfdcd1a17b92559d2f5fd8052a4eeace1
Popup plugin <popup.plugin@gmail.com>
2013-09-28 20:59:00 +0200

    don't crash if the user configured another editor than CK edit to be the default editor

--- src/main/webapp/js/ckeditor-config.js
+++ src/main/webapp/js/ckeditor-config.js
@@ -1,16 +1,18 @@
-CKEDITOR.config.extraPlugins = 'placeholders';
+if (CKEDITOR) {
+    CKEDITOR.config.extraPlugins = 'placeholders';
 
-CKEDITOR.config.toolbar_popups = [
-    ['Styles', 'FontSize', '-', 'TextColor', 'BGColor'],
-    ['Bold', 'Italic', 'Underline', 'StrikeThrough'],
-    ['Subscript', 'Superscript'],
-    '/',
-    ['Undo', 'Redo', '-', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'SelectAll', 'RemoveFormat'],
-    ['Find', 'Replace', 'SpellCheck'],
-    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-    ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-    '/',
-    ['Source'],
-    ['Link', 'Unlink', 'Anchor'],
-    ['Placeholders', 'Image', 'Flash', 'Table', '-', 'Smiley', 'SpecialChar', 'LiferayPageBreak']
-];
+    CKEDITOR.config.toolbar_popups = [
+        ['Styles', 'FontSize', '-', 'TextColor', 'BGColor'],
+        ['Bold', 'Italic', 'Underline', 'StrikeThrough'],
+        ['Subscript', 'Superscript'],
+        '/',
+        ['Undo', 'Redo', '-', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'SelectAll', 'RemoveFormat'],
+        ['Find', 'Replace', 'SpellCheck'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+        '/',
+        ['Source'],
+        ['Link', 'Unlink', 'Anchor'],
+        ['Placeholders', 'Image', 'Flash', 'Table', '-', 'Smiley', 'SpecialChar', 'LiferayPageBreak']
+    ];
+}
