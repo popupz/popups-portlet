0d54bdc5bd44513c3e1455c9e7ea4a72ff91feed
Popup plugin <popup.plugin@gmail.com>
2014-06-08 12:28:16 +0200

    liferay-6.1 specific changes

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,6 +1,6 @@
 var Popups =  {
 
-    _showPopup_liferay_6_1: function(config) {
+    showPopup: function(config) {
         AUI().use('aui-dialog', function (A) {
 
             var dialogButtons = [
@@ -26,54 +26,5 @@ var Popups =  {
 
             dialog.render();
         });
-    },
-    _showPopup: function(config) {
-
-        AUI().use('aui-modal', function (A) {
-
-            var dialogConfig = {
-                bodyContent: config.parseTemplate ? A.Lang.sub(config.bodyContent, config.model) : config.bodyContent,
-                centered: true,
-                headerContent: '<h3>' + config.title + '</h3>',
-                modal: config.mustConfirm,
-                render: '#modal',
-                width: config.width,
-                zIndex: 10000
-            };
-
-            if (config.mustConfirm) {
-                dialogConfig.toolbars = {};
-            }
-
-            var modal = new A.Modal(dialogConfig);
-
-            if (config.mustConfirm) {
-                modal.addToolbar(
-                    [
-                        {
-                            label: config.strings.confirm,
-                            on: {
-                                click: function() {
-                                    if (config.onConfirm) {
-                                        config.onConfirm();
-                                    }
-                                    modal.hide();
-                                }
-                            }
-                        }
-                    ]
-                );
-            }
-
-            modal.render();
-
-        });
-    },
-
-    showPopup: function(config) {
-        var isLiferay61 = AUI.version === '3.4.0'; // there seems to be no easy way to get this info from liferay
-
-        if (isLiferay61) this._showPopup_liferay_6_1(config);
-        else this._showPopup(config);
     }
 };
